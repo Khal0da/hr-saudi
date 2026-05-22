5ed2b49859c8bf525fafb33a5e08682f4868d642
Khaled Elbetaar <38355173+Khal0da@users.noreply.github.com>
2026-05-23 02:37:02 +0300

    Fix setup.py with full package config

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-name = "hr_saudi"
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+setup(
+	name="hr_saudi",
+	version="1.0.0",
+	description="Enterprise Workforce Platform for Construction & Hotel Operations on ERPNext",
+	author="Al Metaeb Investment",
+	author_email="info@almetaeb.com.sa",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires,
+)
